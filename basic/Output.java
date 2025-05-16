400dfef12d3a0a341ba60c0af8a56936683e8c7e
Vaibhav-12345 <vibhav>
2025-05-16 17:32:29 +0530

    println or print difference

--- basic/Output.java
+++ basic/Output.java
@@ -1,6 +1,21 @@
 public  class  Output {
     public static void main(String[] args){
+        System.out.print("hello");
+        System.out.print("hello world\n");
+
+        // Output : hellohello world
+
+
         System.out.println("hello");
         System.out.println("hello world");
+
+         /* 
+         Output : hello
+                  hello world 
+         */
+
+            
+
+
     }
 }
